c220e23b5e2086e7ae2e407d9e6ad7ae1440b5aa
Isaqueu Afonso <isaquelmascarenhas6@gmail.com>
2025-07-03 15:46:37 -0300

    I completed the Challenge: "Python Wisdom Oracle"

---
+++ 05 - Oráculo da sabedoria Python/index.py
@@ -0,0 +1,12 @@
+import os
+os.system('clear')
+
+print('Olá sou seu Oráculo da sabedoria de programação')
+print()
+comando = input('Em que assunto posso lhe ajudar? ')
+
+match comando:
+  case 'Funções em Python':
+    print('Funções em Python permitem organizar o código em blocos reutilizáveis, facilitando a manutenção e a leitura. Você pode definir uma função usando a palavra-chave def e chamá-la sempre que precisar executar aquela tarefa específica.')
+  case _:
+    print('Desculpe ainda estou aprendendo')
