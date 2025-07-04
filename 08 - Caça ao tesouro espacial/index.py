8a5c19f1075d7f2ac591e086bb96f165438287b6
Isaqueu Afonso <isaquelmascarenhas6@gmail.com>
2025-07-04 14:55:47 -0300

    I completed the challenge:
    Space Treasure Hunt

--- 08 - Caça ao tesouro espacial/index.py
+++ 08 - Caça ao tesouro espacial/index.py
@@ -20,11 +20,42 @@ def showMap():
 while True:
   numberRandomOne = random.randint(0, 3)
   numberRandomTwo = random.randint(0, 3)
+  attempt = 1
 
-  for attempt in range(1,16):
-    line = int(input("Digite uma linha: "))
-    column = int(input("Digite uma coluna: "))
-    map[line][column] = player
-    showMap()
+  while attempt <= 15:
+    try:
+      line = int(input("Digite uma linha: "))
+      column = int(input("Digite uma coluna: "))
+      print()
+
+      if line < 0 or line > 3 or column < 0 or column > 3:
+        print('Somente números de 0 a 3 por favor')
+        showMap()
+        continue
+
+      if map[line][column] == player:
+        print("Você já tentou esse, tenta outro!")
+        showMap()
+        continue
+
+      if line == numberRandomOne and column == numberRandomTwo:
+        map[line][column] = treasure
+        showMap()
+        print("Parabéns você encontrou o tesouro!!!!")
+        break
+      else:
+        map[line][column] = player
+        print('Ainda não foi dessa vez tente novamente')
+        showMap()
+        attempt += 1
+      
+      if attempt == 15:
+        map[numberRandomOne][numberRandomTwo] = treasure
+        print('Fim de jogo! O tesouro estava aqui!!!!')
+        showMap()
+    except ValueError:
+      print('Somente números de 0 a 3, Por Favor...')
+      showMap()
+      continue
   
   break
